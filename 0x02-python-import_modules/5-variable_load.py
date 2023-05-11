4ce5e572df88581dd6d505f26ae93c818f85a3e1
Sina Fantu <sinafantu10@gmail.com>
2023-05-11 05:43:32 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-# 3-infinite_add.py
-# Brennan D Baraban <375@holbertonschool.com>
+# 5-variable_load.py
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    print(a)
