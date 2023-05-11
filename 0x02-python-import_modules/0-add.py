d7801b0194b1d6ddafb51d7a502cdeb181039fa2
Sina Fantu <sinafantu10@gmail.com>
2023-05-11 05:28:22 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# Import the add function from add_0.py
+from add_0 import add
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Perform the addition using the add function and store the result
+result = add(a, b)
+
+# Print the result using string formatting
+print("{} + {} = {}".format(a, b, result))
