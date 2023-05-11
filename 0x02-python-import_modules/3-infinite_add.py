a48b241687c2eb893870b26c7dd22b499db083f2
Sina Fantu <sinafantu10@gmail.com>
2023-05-11 05:38:37 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# 3-infinite_add.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 if __name__ == "__main__":
-    """Print the addition of all arguments"""
+    """Print the addition of all arguments."""
     import sys
 
     total = 0
