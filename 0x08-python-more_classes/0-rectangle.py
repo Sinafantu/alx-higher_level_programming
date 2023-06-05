6492888ccaedbeb4e336876381d42f5d53df5845
Sina Fantu <sinafantu10@gmail.com>
2023-06-05 12:13:27 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
 class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
-
-    def perimeter(self):
-        return 2 * (self.length + self.width)
+    """An empty class that represents a rectangle"""
+    pass
